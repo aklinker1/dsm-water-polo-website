7514d53ba2fabec8b924ebc560773547e339ee07
Aaron <aaronklinker1@gmail.com>
2025-03-12 23:56:54 -0500

    fix(server): Remove template routes

--- server/main.ts
+++ server/main.ts
@@ -1,10 +1,6 @@
 import { Elysia } from "elysia";
 import { fetchStatic } from "@aklinker1/aframe/server";
 
-const app = new Elysia()
-  .get("/", "Hello Elysia")
-  .get("/user/:id", ({ params }) => params.id)
-  .post("/form", ({ body }) => body)
-  .mount(fetchStatic());
+const app = new Elysia().mount(fetchStatic());
 
 export default app;
