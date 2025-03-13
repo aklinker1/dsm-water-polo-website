4f2fc3374595fac51a04abcb0281e839912a437a
Aaron <aaronklinker1@gmail.com>
2025-03-12 22:46:02 -0500

    feat: Basic contact page

--- app/pages/Contact.vue
+++ app/pages/Contact.vue
@@ -1,3 +1,15 @@
 <template>
-  <div class="pt-main-navigation">Contact</div>
+  <div
+    class="mt-main-navigation h-[70vh] p-16 flex flex-col gap-8 items-center justify-center text-center max-w-3xl mx-auto"
+  >
+    <h2 class="text-4xl font-poppins font-bold text-primary">Contact Us</h2>
+    <p class="text-lg">
+      Have questions about practice, tournaments, or anything else?
+      <br />
+      Send an email to
+      <a class="link" href="mailto:dsmwaterpolo@gmail.com"
+        >dsmwaterpolo@gmail.com</a
+      >.
+    </p>
+  </div>
 </template>
