d43f971eecc521b974b7f26c9d645a99ad44c407
Aaron <aaronklinker1@gmail.com>
2025-03-14 22:53:09 -0500

    fix: Enable gzip compression for text files

--- aframe.config.ts
+++ aframe.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 import { imagetools } from "vite-imagetools";
 import { ViteImageOptimizer as imageOptimzer } from "vite-plugin-image-optimizer";
+import { compression } from "vite-plugin-compression2";
 
 export default defineConfig({
   prerenderedRoutes: ["/", "/contact", "/events", "/practice"],
@@ -10,6 +11,10 @@ export default defineConfig({
     plugins: [
       vue(),
       tailwindcss(),
+      // Compress JS and CSS files
+      compression({
+        exclude: [/\.html$/],
+      }),
       // Convert images to WEBP
       imagetools(),
       // Compress SVGs
