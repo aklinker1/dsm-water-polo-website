036154e308d15c88c3db2fe00f7a34fcdbac2037
Aaron <aaronklinker1@gmail.com>
2025-05-03 14:44:25 -0500

    feat: Fill out practice page

--- app/pages/Practice.vue
+++ app/pages/Practice.vue
@@ -1,3 +1,15 @@
 <template>
-  <div class="pt-main-navigation">Practice</div>
+  <div class="mt-main-navigation p-16 flex flex-col gap-16 max-w-6xl mx-auto">
+    <h2 class="text-4xl font-poppins font-bold text-primary">Practice</h2>
+    <div class="flex flex-col gap-4">
+      <h3 class="text-2xl font-bold font-poppins">Des Moines, IA</h3>
+      <p><i>Fall, Winter, Spring</i>: Tue/Thu 8-9pm @ Wellmark YMCA (indoor)</p>
+      <p>Summer: Tue/Thu 8-9:30pm, Sat 10-11:30am @ Ashworth (outdoor)</p>
+      <!-- <p>See our Google Calendar for exact start/end dates of summer practices</p> -->
+    </div>
+    <div class="flex flex-col gap-4">
+      <h3 class="text-2xl font-bold font-poppins">Ames, IA</h3>
+      <p>All year: Mon/Thu 7:45-8:45pm @ Ames High School (indoor)</p>
+    </div>
+  </div>
 </template>
