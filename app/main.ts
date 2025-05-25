ee6da0c375b354cef4217795b36ac4bfcd03a92b
Aaron <aaronklinker1@gmail.com>
2025-05-24 23:34:24 -0500

    fix: Additional performance metrics

--- app/main.ts
+++ app/main.ts
@@ -56,8 +56,36 @@ createApp(RouterView).use(router).mount(document.body);
 
 performance.mark("app-mounted-end");
 
-waitForUmami(() => {
-  umami.track("performance", {
-    interactive: performance.getEntriesByName("app-mounted-end")[0].startTime,
+// Report performance metrics
+const observer = new PerformanceObserver((list) => {
+  waitForUmami(() => {
+    const nav = list.getEntriesByType(
+      "navigation",
+    )[0] as PerformanceNavigationTiming;
+
+    if (nav.duration === 0) return;
+
+    const appMountedStart =
+      performance.getEntriesByName("app-mounted-start")[0].startTime;
+    const appMountedEnd =
+      performance.getEntriesByName("app-mounted-end")[0].startTime;
+
+    const data = {
+      // When the readyState is set to interactive, this means the DOM has been parsed and is ready for manipulation
+      "dom-interactive": nav.domInteractive,
+
+      // The time the vue app was mounted and the page is ready for user interactions
+      "app-mounted": appMountedEnd,
+
+      // How long the JS took to mount the view app
+      "app-mount-duration": appMountedEnd - appMountedStart,
+
+      // Total time for the browser to load all the resources
+      "total-load-time": nav.duration,
+    };
+    (import.meta.env.PROD ? umami.track : console.log)("performance", data);
+
+    observer.disconnect();
   });
 });
+observer.observe({ type: "navigation", buffered: true });
