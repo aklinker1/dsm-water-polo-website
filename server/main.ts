67c35be9eb9a90b1f077b6c11444241c7f81a2f0
Aaron <aaronklinker1@gmail.com>
2025-05-24 22:48:53 -0500

    fix: Add logs for HTTP requests

--- server/main.ts
+++ server/main.ts
@@ -3,6 +3,26 @@ import { fetchStatic } from "@aklinker1/aframe/server";
 
 const api = new Elysia({ prefix: "/api" }).get("/health", () => {});
 
-const app = new Elysia().mount(fetchStatic()).use(api);
+const app = new Elysia()
+  .onRequest((ctx) => {
+    // @ts-expect-error: Adding untyped property to ctx
+    ctx.startTime = performance.now();
+  })
+  .onAfterResponse(
+    // @ts-expect-error: startTime is not typed
+    ({ request, route, set, path, startTime }) => {
+      console.log(
+        JSON.stringify({
+          route,
+          status: set.status,
+          duration: performance.now() - startTime,
+          method: request.method,
+          path,
+        }),
+      );
+    },
+  )
+  .mount(fetchStatic())
+  .use(api);
 
 export default app;
