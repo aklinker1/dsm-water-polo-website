ed895bafc2374ac0b58ea03c081f57cb7fa1e758
Aaron <aaronklinker1@gmail.com>
2025-05-24 23:46:18 -0500

    chore: Use promise for waiting for umami

--- app/main.ts
+++ app/main.ts
@@ -57,35 +57,35 @@ createApp(RouterView).use(router).mount(document.body);
 performance.mark("app-mounted-end");
 
 // Report performance metrics
-const observer = new PerformanceObserver((list) => {
-  waitForUmami(() => {
-    const nav = list.getEntriesByType(
-      "navigation",
-    )[0] as PerformanceNavigationTiming;
+const observer = new PerformanceObserver(async (list) => {
+  const nav = list.getEntriesByType(
+    "navigation",
+  )[0] as PerformanceNavigationTiming;
 
-    if (nav.duration === 0) return;
+  if (nav.duration === 0) return;
 
-    const appMountedStart =
-      performance.getEntriesByName("app-mounted-start")[0].startTime;
-    const appMountedEnd =
-      performance.getEntriesByName("app-mounted-end")[0].startTime;
+  const appMountedStart =
+    performance.getEntriesByName("app-mounted-start")[0].startTime;
+  const appMountedEnd =
+    performance.getEntriesByName("app-mounted-end")[0].startTime;
 
-    const data = {
-      // When the readyState is set to interactive, this means the DOM has been parsed and is ready for manipulation
-      "dom-interactive": nav.domInteractive,
+  const data = {
+    // When the readyState is set to interactive, this means the DOM has been parsed and is ready for manipulation
+    "dom-interactive": nav.domInteractive,
 
-      // The time the vue app was mounted and the page is ready for user interactions
-      "app-mounted": appMountedEnd,
+    // The time the vue app was mounted and the page is ready for user interactions
+    "app-mounted": appMountedEnd,
 
-      // How long the JS took to mount the view app
-      "app-mount-duration": appMountedEnd - appMountedStart,
+    // How long the JS took to mount the view app
+    "app-mount-duration": appMountedEnd - appMountedStart,
 
-      // Total time for the browser to load all the resources
-      "total-load-time": nav.duration,
-    };
-    (import.meta.env.PROD ? umami.track : console.log)("performance", data);
+    // Total time for the browser to load all the resources
+    "total-load-time": nav.duration,
+  };
 
-    observer.disconnect();
-  });
+  const umami = await waitForUmami();
+  (import.meta.env.PROD ? umami.track : console.log)("performance", data);
+
+  observer.disconnect();
 });
 observer.observe({ type: "navigation", buffered: true });
--- app/utils/analytics.ts
+++ app/utils/analytics.ts
@@ -1,13 +1,15 @@
 /** Wait for the globalThis.umami to be defined. Stop waiting after 5 seconds. Check every second. */
-export function waitForUmami(cb: () => void): void {
-  let tries = 1;
-  const checkUmami = () => {
-    if (globalThis.umami) return cb();
-    if (tries >= 5) return;
+export function waitForUmami(): Promise<typeof umami> {
+  return new Promise((resolve, reject) => {
+    let tries = 1;
+    const checkUmami = () => {
+      if (globalThis.umami) return resolve(globalThis.umami);
+      if (tries >= 5) return reject(new Error("Umami not found"));
 
-    tries++;
-    setTimeout(checkUmami, 1000);
-  };
+      tries++;
+      setTimeout(checkUmami, 1000);
+    };
 
-  checkUmami();
+    checkUmami();
+  });
 }
