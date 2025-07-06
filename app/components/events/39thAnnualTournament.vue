648ef69181e56aecfe37c9c01c8d812788abcfb9
Aaron <aaronklinker1@gmail.com>
2025-07-06 01:46:42 -0500

    fix: Update tournament details

--- app/components/events/39thAnnualTournament.vue
+++ app/components/events/39thAnnualTournament.vue
@@ -4,12 +4,11 @@ import poolAshworth from "../../assets/pool-ashworth";
 
 <template>
   <img class="w-full" :srcset="poolAshworth" alt="Ashworth Pool" />
+
   <h2 class="font-poppins font-medium text-lg text-primary">
     Participating Teams
   </h2>
-  <p>
-    The tournament usually has 10-12 teams participating. This year we have:
-  </p>
+  <p>This year there are 10 teams participating:</p>
   <ul class="list-disc ml-8">
     <li>Polo Bears (Des Moines) - 2 teams</li>
     <li>Common Loons (Minnesota) - 2 teams</li>
@@ -20,10 +19,29 @@ import poolAshworth from "../../assets/pool-ashworth";
     <li>Apostles (Chicago)</li>
     <li>Club of Chicago</li>
   </ul>
+  <p>
+    See
+    <a
+      class="link"
+      href="https://docs.google.com/spreadsheets/d/1V63r5BCOX_LoDCBrVk0B9yAs7O4eLfpTC1x8oO0gIQY/edit?usp=sharing"
+      >schedule and results</a
+    >
+    for more details.
+  </p>
+
   <h2 class="font-poppins font-medium text-lg text-primary">
     Registration Fees
   </h2>
   <p>
+    <b>Registration is closed</b>. See
+    <a
+      class="link"
+      href="https://docs.google.com/spreadsheets/d/1V63r5BCOX_LoDCBrVk0B9yAs7O4eLfpTC1x8oO0gIQY/edit?usp=sharing"
+      >schedule and results</a
+    >
+    for info on participating teams.
+  </p>
+  <p class="line-through">
     If your team is interested in participating, contact us at
     <a class="link" href="mailto:dsmwaterpolo@gmail.com"
       >dsmwaterpolo@gmail.com</a
@@ -32,10 +50,10 @@ import poolAshworth from "../../assets/pool-ashworth";
     we can help you find a team.
   </p>
   <ul class="list-disc ml-8">
-    <li>Team: $650</li>
-    <li>Individual: $50</li>
+    <li class="line-through">Team: $650</li>
+    <li class="line-through">Individual: $50</li>
   </ul>
-  <p>
+  <p class="line-through">
     There are limited spots available, and we give priority to teams that have
     participated in the past, so send us an email if you're interested in
     participating and we'll let you know if there's still room left.
