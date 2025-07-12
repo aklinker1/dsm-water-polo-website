443fb682eebb582179a206ebd58f04d17fb0a3fe
Aaron <aaronklinker1@gmail.com>
2025-07-12 11:47:51 -0500

    fix: Upgrade aframe to fix flickering

--- aframe.config.ts
+++ aframe.config.ts
@@ -1,18 +1,12 @@
 import { defineConfig } from "@aklinker1/aframe";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
-import { compression } from "vite-plugin-compression2";
 import { imageMinifier } from "./plugins/image-minifier";
 
 export default defineConfig({
   prerenderedRoutes: ["/", "/contact", "/events", "/practice"],
   vite: {
-    plugins: [
-      vue(),
-      tailwindcss(),
-      compression({ exclude: [/\.html$/] }), // Compress JS and CSS files
-      imageMinifier(),
-    ],
+    plugins: [vue(), tailwindcss(), imageMinifier()],
   },
   prerenderer: {
     waitForSelector: "body > *",
